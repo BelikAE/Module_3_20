d461fdb4df52567dd1d037fdf3294847804aa27c
BelikAE <BelikAE@HOME-PC>
2024-07-25 20:59:48 +1000

    Выдача

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,8 +8,35 @@ namespace Lab1
 {
     internal class Program
     {
+        delegate double MyDelegate(double r);
         static void Main(string[] args)
         {
+            MyDelegate myDelegate = D;
+            myDelegate += S;
+            myDelegate += V;
+            myDelegate(3);
+            Console.ReadKey();
+        }
+
+        static double D(double r) 
+        {
+            double d = 2*Math.PI*r;
+            Console.WriteLine(d);
+            return d;
+        }
+
+        static double S(double r)
+        {
+            double s =  Math.PI * Math.Pow(r, 2);
+            Console.WriteLine(s);
+            return s;
+
+        }
+        static double V(double r)
+        {
+            double v = 4.00/3*Math.PI * Math.Pow(r,3);
+            Console.WriteLine(v);
+            return v;
         }
     }
 }
